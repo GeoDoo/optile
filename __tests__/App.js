4275e3633c5bab98e864f9dc4d7421b1812b6179
Georgios Karametas <geometas@hotmail.com>
2017-11-26 01:18:35 +0100

    Fixed App.test.js with mocking Camera and configuring Jest in package.json file

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,12 +1,14 @@
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import 'react-native'
+import React from 'react'
+import App from '../App'
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
-it('renders correctly', () => {
+jest.mock('react-native-camera', () => require.requireActual('../__mocks__/react-native-camera'))
+
+test('renders correctly', () => {
   const tree = renderer.create(
     <App />
-  );
-});
+  )
+})
